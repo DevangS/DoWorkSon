d3cbc83ccfbf04b832cc122ccd29715c2a0c2733
Devin <devin@devin-VirtualBox.(none)>
2013-01-11 15:30:02 -0800

    initialize me cap'n

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoWorkSon::Application.config.secret_token = '864629d95f4e09f7a3e021e17fde53f2ce703151b29046c0c147e753495757515f65f68cde1d72c3b6aa0fda0c1f9031183446529fdce44ae41e162d36a54817'
